3eab6b83083f3e23ec47c37f6d3f2c08c728bbfb
Артем Рубан <ruban@sports.ru>
2017-08-13 18:16:16 +0300

    Замена текстов на главной

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="text">
-        MGCom занимается перфоманс-маркетингом для крупного и среднего бизнеса:
-        <router-link to="/contextus" class="link">контекстная</router-link> и
-        <router-link to="/media" class="link">медийная реклама</router-link>,
-        <router-link to="/target" class="link">таргетированная реклама в социальных сетях</router-link>,
+        Агентство помогает крупному и среднему бизнесу привлекать клиентов и проводить их по воронке,
+        увеличивать продажи и охват, повышать знание и лояльность – с помощью перфоманс-маркетинга – комбинируя разные инструменты:
+        <router-link to="/contextus" class="link">контекстную</router-link> и
+        <router-link to="/media" class="link">медийную рекламу</router-link>,
+        <router-link to="/target" class="link">таргетированную рекламу в соцсетях</router-link>,
         <router-link to="/cpa" class="link">CPA</router-link> и
-        <router-link to="/price-lists" class="link">прайс-площадки</router-link>,
-        <router-link to="/seo" class="link">SEO</router-link> и
-        <router-link to="/analitycs" class="link">веб-аналитика</router-link>,
-        <router-link to="/mobile-ads" class="link">мобильная реклама</router-link>.
-        <br><br>
-        Умеем работать и с масштабными проектами и digital-стратегиями, и с отдельными задачами.
+        <router-link to="/price-lists" class="link">маркетплейсы</router-link>,
+        <router-link to="/analitycs" class="link">аналитику</router-link> и
+        <router-link to="/seo" class="link">SEO</router-link>, на десктопе и в
+        <router-link to="/mobile-ads" class="link">мобильном</router-link>.
+        Умеем работать с масштабными проектами, стратегиями и отдельными задачами.
     </div>
 </template>
 
