c6c9a8dcdd00a173b702e769eb29cb59e3ca093d
Jmunb <rubartem@gmail.com>
2017-09-11 19:16:03 +0300

    Изменены глобальные отступы сайта и элементов шапки и подвала Страница вакансий

---
+++ src/components/oneJob.vue
@@ -0,0 +1,124 @@
+<template>
+    <li class="one-job">
+        <div class="one-job__num">{{ index }} / {{ jobCount }}</div>
+        <div class="one-job__title">{{ job.title }}</div>
+        <div class="one-job__text">{{ job.text }}</div>
+        <div class="one-job__send-block">
+            <div v-if="showForm" class="one-job__form">
+                <div class="one-line">
+                    <div class="one-line__label">От:</div>
+                    <input name="email" type="email" class="one-line__input" placeholder="test@qweqwe.com">
+                </div>
+                <div class="one-line">
+                    <div class="one-line__label"></div>
+                    <textarea name="text" id="" class="one-line__textarea" placeholder="Представьтесь и напишите несколько слов о себе"></textarea>
+                </div>
+                <div class="one-line one-line--last">
+                    <div class="one-line__label one-line__label--last">+</div>
+                    <div class="one-line__file">Приложить файл</div>
+                </div>
+            </div>
+            <a v-else href="javascript:void(0)" class="one-job__button" @click.stop.prevent="showForm=true">Отправить резюме</a>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'one-job',
+  props: ['job', 'jobCount', 'index'],
+  data () {
+    return {
+      showForm: false
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.one-line
+    display: flex
+    flex-flow: row nowrap
+    justify-content: flex-start
+    font-size: 1rem
+    letter-spacing: 0.038rem
+
+    &__label
+        flex: 0 0 2.5rem
+        text-align: right
+
+        &--last
+            padding-right: .5rem
+
+    &__input
+        border: 0
+        color: #0A0AE2
+        outline: 0
+        flex-grow: 1
+        text-indent: 3px
+        padding: 0
+        margin: -5px 0 0 0
+        background: transparent
+
+    &__textarea
+        flex: 0 0 50%
+        border: 0
+        outline: 0
+        margin: .7rem 0 0 0
+        padding: 0 0 0 .5rem
+        resize: none
+        background: transparent
+        height: 9.5rem
+
+    &__file
+        cursor: pointer
+        padding-left: .6rem
+
+    &--last
+        font-size: 1.2rem
+        margin-top: .3rem
+
+.one-job
+    margin-top: 5.5rem
+    font-size: 1.3rem
+    letter-spacing: 0.05rem
+    line-height: 2rem
+
+    &:first-of-type
+        margin-top: 0
+
+    &__title
+        font-size: 2.8rem
+        letter-spacing: 0
+        line-height: 4rem
+        margin-top: 3.5rem
+
+    &__text
+        margin-top: 1.9rem
+
+    &__send-block
+        margin-top: 2.8rem
+
+    &__form
+        padding: 2rem 1.6rem
+        width: 35.2rem
+        height: 18.7rem
+        overflow: hidden
+        background: #F8F8FB
+        border-radius: 2rem
+
+    &__button
+        font-size: 1rem
+        line-height: 4rem
+        background: #F8F8FB
+        border-radius: 2rem
+        display: inline-block
+        text-decoration: none
+        padding: 0 1.6rem
+        color: rgba(0, 0, 0, .4)
+        letter-spacing: 0.038rem
+        transition: color .1s linear
+
+        &:hover
+            color: rgba(0, 0, 0, 1)
+</style>
--- src/pages/Vacancies.vue
+++ src/pages/Vacancies.vue
@@ -1,37 +1,223 @@
 <template>
-  <div class="hello">
-    Вакансии
+  <div class="wrapper">
+    <div class="scroller">
+      <div class="content">
+        <div class="tags">
+          <div class="tags-column">
+            <div class="one-tag__wrapper">
+              <a
+                  href="#"
+                  class="one-tag"
+                  v-bind:class="{'one-tag--disabled': tags[0].isDisabled, 'one-tag--active': !tags[0].isDisabled && tags[0].isActive}"
+                  @click.stop.prevent="setTagState(0)"
+              >{{ tags[0].title }}</a>
+            </div>
+            <div class="one-tag__wrapper">
+              <a
+                      href="#"
+                      class="one-tag"
+                      v-bind:class="{'one-tag--disabled': tags[1].isDisabled, 'one-tag--active': !tags[1].isDisabled && tags[1].isActive }"
+                      @click.stop.prevent="setTagState(1)"
+              >{{ tags[1].title }}</a>
+            </div>
+          </div>
+          <div class="tags-column">
+            <div class="one-tag__wrapper">
+              <a
+                      href="#"
+                      class="one-tag"
+                      v-bind:class="{'one-tag--disabled': tags[2].isDisabled, 'one-tag--active': !tags[2].isDisabled && tags[2].isActive }"
+                      @click.stop.prevent="setTagState(2)"
+              >{{ tags[2].title }}</a>
+            </div>
+            <div class="one-tag__wrapper">
+              <a
+                      href="#"
+                      class="one-tag"
+                      v-bind:class="{'one-tag--disabled': tags[3].isDisabled, 'one-tag--active': !tags[3].isDisabled && tags[3].isActive }"
+                      @click.stop.prevent="setTagState(3)"
+              >{{ tags[3].title }}</a>
+            </div>
+          </div>
+          <div class="tags-column">
+            <div class="one-tag__wrapper">
+              <a
+                      href="#"
+                      class="one-tag"
+                      v-bind:class="{'one-tag--disabled': tags[4].isDisabled, 'one-tag--active': !tags[4].isDisabled && tags[4].isActive }"
+                      @click.stop.prevent="setTagState(4)"
+              >{{ tags[4].title }}</a>
+            </div>
+          </div>
+          <div class="tags-column">
+            <div class="one-tag__wrapper">
+              <a
+                      href="#"
+                      class="one-tag"
+                      v-bind:class="{'one-tag--disabled': tags[5].isDisabled, 'one-tag--active': !tags[5].isDisabled && tags[5].isActive }"
+                      @click.stop.prevent="setTagState(5)"
+              >{{ tags[5].title }}</a>
+            </div>
+            <div class="one-tag__wrapper">
+              <a
+                      href="#"
+                      class="one-tag"
+                      v-bind:class="{'one-tag--disabled': tags[6].isDisabled, 'one-tag--active': !tags[6].isDisabled && tags[6].isActive }"
+                      @click.stop.prevent="setTagState(6)"
+              >{{ tags[6].title }}</a>
+            </div>
+          </div>
+        </div>
+        <ul class="jobs-list">
+          <one-job
+            v-for="(job, index) in filterVacancies"
+            :key="job.id"
+            :job="job"
+            :index="index + 1"
+            :jobCount="filterVacancies.length"
+          ></one-job>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import OneJob from '../components/oneJob.vue'
+  import vacancies from '../vacancies.json'
+  import vacanciesTags from '../vacancies-tags.json'
+
+  function IntersecArrays (A = [], B = []) {
+    let m = A.length
+    let n = B.length
+    let c = 0
+    let C = []
+
+    for (let i = 0; i < m; i++) {
+      let j = 0
+      let k = 0
+
+      while (B[j] !== A[ i ] && j < n) j++
+      while (C[k] !== A[ i ] && k < c) k++
+      if (j !== n && k === c) C[c++] = A[ i ]
+    }
+    return C
+  }
+
+  export default {
+    name: 'Vacancies',
+    data () {
+      vacanciesTags.forEach(item => {
+        item.isActive = false
+        item.isDisabled = false
+      })
+
+      return {
+        vacancies: vacancies,
+        tags: vacanciesTags
+      }
+    },
+    computed: {
+      filterVacancies () {
+        let activeIds = []
+
+        this.tags.forEach(item => {
+          if (item.isActive) {
+            activeIds.push(item.id)
+          }
+        })
+
+        return !activeIds.length ? this.vacancies : this.vacancies.filter(item => {
+          return IntersecArrays(item.tags, activeIds).length
+        })
+      }
+    },
+    methods: {
+      setTagState (index) {
+        let excludeIds = []
+
+        this.tags[index].isActive = !this.tags[index].isActive
+
+        this.tags.forEach(item => {
+          if (item.isActive) {
+            Array.prototype.push.apply(excludeIds, item.exclude)
+          }
+        })
+
+        excludeIds = Array.from(new Set(excludeIds))
+
+        this.tags.forEach(item => {
+          if (excludeIds.indexOf(item.id) > -1) {
+            item.isActive = false
+            item.isDisabled = true
+          } else {
+            item.isDisabled = false
+          }
+        })
+      }
+    },
+    components: {
+      OneJob
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="sass" scoped>
+  .wrapper
+    flex-grow: 1
+    display: flex
+    background: #fff
+    overflow: hidden
+    position: relative
+    flex-flow: row nowrap
+    align-items: flex-start
+    width: calc(100% - 50.2rem)
+    margin: -10.4rem 0 0 23.7rem
+    border-radius: 0 0 .7rem .7rem
+    justify-content: space-between
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .scroller
+    overflow: auto
+    // width: calc(100% + 10rem)
+    height: calc(100vh - 17.8rem)
+    -webkit-overflow-scrolling: touch
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .content
+    padding: 6.9rem 6.5rem 4.7rem
 
-a {
-  color: #42b983;
-}
+
+  .one-tag
+    cursor: pointer
+    font-size: 1.3rem
+    letter-spacing: 0.05rem
+    line-height: 2rem
+
+    &--active
+      color: #0A0AE2
+
+    &--disabled
+      opacity: 0.27
+      pointer-events: none
+
+    &__wrapper
+      min-height: 4rem
+      margin-bottom: 2.5rem
+
+  .tags
+    display: flex
+    flex-flow: row nowrap
+    justify-content: space-between
+
+    &-column
+      flex: 0 0 25%
+      display: flex
+      padding-right: 2rem
+      flex-flow: column nowrap
+      justify-content: flex-end
+
+  .jobs-list
+    width: 75%
+    padding: 0
+    list-style: none
+    margin: 1.4rem 0 0 0
 </style>
